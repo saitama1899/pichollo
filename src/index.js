ba3256dba4baa34178fb59b8068338491ca4f8e4
saitama1899 <eric.lorien92@gmail.com>
2022-02-05 19:18:25 +0100

    Port env fix to deploy

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use('/api/notifications', notificationsRouter)
 // Static content
 app.use(express.static(path.join(__dirname, '/public')))
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
